aeb82a535dcdf11d0a9f5f6989a43afcb6a6fa9a
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-08-18 20:34:25 +0200

    Initial commit

---
+++ src/main/java/me/petrov/algorithmik/Algorithmik.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.petrov.algorithmik;
+
+import me.petrov.algorithmik.helloworld.*;
+
+
+/**
+ *
+ * @author Samuil Petrov
+ * @author Marcos Esteban Gorena
+ */
+public class Algorithmik {
+
+    public static void main(String[] args) {
+        HelloWorldJFrame gui = new HelloWorldJFrame();
+        gui.setVisible(true);
+    }
+}
