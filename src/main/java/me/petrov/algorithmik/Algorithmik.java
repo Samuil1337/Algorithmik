d2d15a7d68563281c050bb2610de773e475bca76
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-08-23 13:21:55 +0200

    Start CalculatorJFrame by default

--- src/main/java/me/petrov/algorithmik/Algorithmik.java
+++ src/main/java/me/petrov/algorithmik/Algorithmik.java
@@ -6,6 +6,8 @@ package me.petrov.algorithmik;
 
 import me.petrov.algorithmik.helloworld.*;
 import me.petrov.algorithmik.converter.*;
+import me.petrov.algorithmik.roulette.*;
+import me.petrov.algorithmik.calculator.*;
 
 
 /**
@@ -17,7 +19,9 @@ public class Algorithmik {
 
     public static void main(String[] args) {
         //HelloWorldJFrame gui = new HelloWorldJFrame();
-        ConverterJFrame gui = new ConverterJFrame();
+        //ConverterJFrame gui = new ConverterJFrame();
+        //RouletteJFrame gui = new RouletteJFrame();
+        CalculatorJFrame gui = new CalculatorJFrame();
         gui.setVisible(true);
     }
 }
