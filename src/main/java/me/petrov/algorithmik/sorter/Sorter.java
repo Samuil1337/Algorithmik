f4229b9967151dc0abb898e773bf50044dbbddfa
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-09-23 13:05:30 +0200

    Sorter: Add selection sort

--- src/main/java/me/petrov/algorithmik/sorter/Sorter.java
+++ src/main/java/me/petrov/algorithmik/sorter/Sorter.java
@@ -10,22 +10,28 @@ package me.petrov.algorithmik.sorter;
  */
 public class Sorter {
     
+    private static int compareCounter = 0;
+    
     public static void main(String args[]) {
-        printArray(insertSort(new int[]{ 2, 6, 3, 7, 1, 5 }));
+        printArray(selectionSort(new int[]{ 2, 6, 3, 7, 1 }));
     }
     
     public static int[] insertSort(int[] numbers) {
+        compareCounter = 0;
         int n = numbers.length;
         for (int i = 1; i != n; i++) {
             int element = numbers[i];
             int j = 0;
             while (numbers[j] < element) {
                 j++;
+                compareCounter++;
             }
             for (int k = i; k != j; k--) {
                 numbers[k] = numbers[k - 1];
+                compareCounter++;
             }
             numbers[j] = element;
+            compareCounter++;
         }
         
         return numbers;
@@ -45,6 +51,34 @@ public class Sorter {
         }
         
         return numbers;
+    }
+    
+    public static int[] selectionSort(int[] numbers) {
+        compareCounter = 0;
+        for (int i = numbers.length - 1; i > 0; i--) {
+            int maxIndex = 0;
+            int max = numbers[maxIndex];
+            for (int j = 0; j <= i; j++) {
+                if (numbers[j] > max) {
+                    maxIndex = j;
+                    max = numbers[maxIndex];
+                }
+                compareCounter += 2;
+            }
+            numbers[maxIndex] = numbers[i];
+            numbers[i] = max;
+            
+            compareCounter++;
+        }
+        System.out.println(compareCounter);
+        return numbers;
+    }
+    
+    public static String getBestSorterOnAverage() {
+        int[][] arrays = new int[10][];
+        for (int i = 0; i < arrays.length; i++) {
+            
+        }
     }
     
     public static void printArray(int[] array) {
