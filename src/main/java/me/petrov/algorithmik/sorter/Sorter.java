2b4b75340355102b3925fe1d4766db2e28112520
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-09-26 00:16:14 +0200

    Sorter: Add benchmark

--- src/main/java/me/petrov/algorithmik/sorter/Sorter.java
+++ src/main/java/me/petrov/algorithmik/sorter/Sorter.java
@@ -4,16 +4,19 @@
  */
 package me.petrov.algorithmik.sorter;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Samuil Petrov
  */
 public class Sorter {
     
-    private static int compareCounter = 0;
+    private static long compareCounter = 0;
     
     public static void main(String args[]) {
-        printArray(selectionSort(new int[]{ 2, 6, 3, 7, 1 }));
+        //printArray(selectionSort(new int[]{ 2, 6, 3, 7, 1 }));
+        testSorters(1);
     }
     
     public static int[] insertSort(int[] numbers) {
@@ -70,14 +73,68 @@ public class Sorter {
             
             compareCounter++;
         }
-        System.out.println(compareCounter);
+        //System.out.println(compareCounter);
         return numbers;
     }
     
     public static String getBestSorterOnAverage() {
-        int[][] arrays = new int[10][];
-        for (int i = 0; i < arrays.length; i++) {
-            
+        // create original array
+        int[][] selectionSortArray = new int[5][];
+        for (int i = 0; i < selectionSortArray.length; i++) {
+            selectionSortArray[i] = new int[(int) Math.pow(10, i + 1)];
+        }
+        // fill original array with random numbers
+        for (int i = 0; i < selectionSortArray.length; i++) {
+            for (int j = 0; j < selectionSortArray[i].length; j++) {
+                selectionSortArray[i][j] = (int) (Math.random() * Integer.MAX_VALUE);
+            }
+        }
+        // create deep copy of original array
+        int[][] insertSortArray = new int[selectionSortArray.length][];
+        for (int i = 0; i < selectionSortArray.length; i++) {
+            insertSortArray[i] = Arrays.copyOf(selectionSortArray[i], selectionSortArray[i].length);
+        }
+        
+        // test selectionSort
+        long selectionCounter = 0;
+        long selectionTimer = System.nanoTime();
+        for (int[] array : selectionSortArray) {
+            selectionSort(array);
+            selectionCounter += compareCounter;
+        }
+        selectionTimer = (System.nanoTime() - selectionTimer) / 1_000_000;
+        // test inserSort
+        long insertCounter = 0;
+        long insertTimer = System.nanoTime();
+        for (int[] array : insertSortArray) {
+            insertSort(array);
+            insertCounter += compareCounter;
+        }
+        insertTimer = (System.nanoTime() - insertTimer) / 1_000_000;
+        
+        // check which one won
+        double insertToSelectRatio = (double)insertCounter / selectionCounter;
+        String chart = 
+                "\nInsert sort compare ops    : " + insertCounter + '\n'
+                + "Insert sort time           : " + insertTimer + " ms\n"
+                + "Selection sort compare ops : " + selectionCounter + '\n'
+                + "Selection sort time        : " + selectionTimer + " ms\n"
+                + "Insert to selection ratio  : " + insertToSelectRatio + '\n';
+        if (insertCounter == selectionCounter) {
+            return "[Both are equal]" + chart;
+        } else if (insertCounter > selectionCounter) {
+            return "[Selection sort is better]" + chart;
+        } else {
+            return "[Insert sort is better]" + chart;
+        }
+    }
+    
+    public static void testSorters(int passes) {
+        System.out.println("Testing each sorting algorithm...");
+        for (int i = 0; i < passes; i++) {
+            System.out.println("Pass #" + (i + 1));
+            System.out.println("--------");
+            System.out.println(getBestSorterOnAverage());
         }
     }
     
