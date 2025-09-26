e620014c7019a9075408523678b55cfca6fe86ee
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-09-26 21:41:25 +0200

    Mathler: Add documentation

--- src/main/java/me/petrov/algorithmik/mathler/RecursiveMath.java
+++ src/main/java/me/petrov/algorithmik/mathler/RecursiveMath.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -14,26 +14,53 @@ public class RecursiveMath {
         //System.out.println(fibonacci(5));
     }
     
-    public static long faculty(long n) {
+    /**
+     * Calculates the factorial of a given number.
+     * 
+     * The factorial of a non-negative integer n, denoted by n!,
+     * is the product of all positive integers less than or equal to n.
+     * By definition, 0! = 1! = 1.
+     * 
+     * @param n the number to calculate the factorial of
+     * @return the factorial of n
+     * @throws IllegalArgumentException if n is less than 0
+     */
+    public static long factorial(long n) {
         if (n < 0) throw new IllegalArgumentException("n can't be less than 0!");
         if (n == 0 || n == 1) return 1;
-        return n * faculty(n -1);
+        return n * factorial(n -1);
     }
     
+    /**
+     * Calculates the nth Fibonacci number.
+     * 
+     * The Fibonacci sequence is a series of numbers where a number is the addition of the last two numbers,
+     * starting with 0 and 1.
+     * 
+     * @param n the position of the Fibonacci number to calculate
+     * @return the nth Fibonacci number
+     * @throws IllegalArgumentException if n is less than 0
+     */
     public static long fibonacci(long n) {
-        // Fibonacci isn't defined for n<0
         if (n < 0) throw new IllegalArgumentException("n can't be less than 0!");
-        // Default cases of Fibonacci
         if (n == 0) return 0;
         if (n == 1) return 1;
-        // Calculate the value by adding the two previous ones (Fibonacci)
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
     
-    public static long greatestCommonDenominator(long a, long b) {
-        if (b < 0) throw new IllegalArgumentException("b can't be less than 0!"); 
+    /**
+     * Calculates the Greatest Common Divisor (GCD)
+     * of two long integers using the Euclidean algorithm.
+     * 
+     * @param a the first non-negative long integer
+     * @param b the second non-negative long integer
+     * @return the GCD of a and b
+     * @throws IllegalArgumentException if either a or b is negative
+     */
+    public static long gcd(long a, long b) {
+        if (a < 0 || b < 0) throw new IllegalArgumentException("a and b can't be less than 0!"); 
         if (b == 0) return a;
-        return greatestCommonDenominator(b, a % b);
+        return gcd(b, a % b);
     }
     
     public static String stringMinusOne(String s) {
